7e6792461c3c2f1240a054b0a28f9f98b5ce4c0b
aagor1414 <aagor1414@gmail.com>
2021-02-04 18:58:00 +0300

    добавил папку lesson_2_78

---
+++ lesson_2_78/ClientApp.java
@@ -0,0 +1,10 @@
+package client;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        new MyClient("user1", "pass1").setVisible(true);
+        //new MyClient("user2", "pass2").setVisible(true);
+        //ew MyClient("user3", "pass3").setVisible(true);
+        //new MyClient().setVisible(true);
+    }
+}
